bdc00e88adf2ef5362f0158e7ce88f211a64481c
Sammantha VonBergen <sammantha.sadler@gmail.com>
2024-11-09 15:04:36 -0700

    fix: build
    Fix one last build warning

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
  */
 const nextConfig = {
   /* config options here */
-  output: 'out' // Change the output directory to 'build'
+  output: '.vercel/output/static' // Change the output directory to 'build'
 }
 
 if (process.env.NODE_ENV === 'development') {
