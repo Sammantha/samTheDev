05484bb6168a261ee3b716923fb87fceef7e7a6e
Sammantha VonBergen <sammantha.sadler@gmail.com>
2024-11-09 08:50:40 -0700

    fix: build
    setting next.config.js instead of .mjs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
+module.exports = {
+  distDir: 'build' // Change the output directory to 'build'
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
  if (process.env.NODE_ENV === 'development') {
    await setupDevPlatform();
  }
-
-  module.exports = {
-    distDir: 'build' // Change the output directory to 'build'
-  }
 
 export default nextConfig;
