bbefbfc3e4e014856f1eb2edc5b5a5c9cd034add
Sammantha VonBergen <sammantha.sadler@gmail.com>
2024-11-12 20:03:06 -0700

    feat: application tracker
    Inserted screenshot of application tracker and data object with descriptions.

--- _api/resources.ts
+++ _api/resources.ts
@@ -1,6 +1,25 @@
 export const data = {
     resources: [
         {
+            id: 2,
+            buttonLink: "https://docs.google.com/spreadsheets/d/1UYiPifu2LD2vCftK3IMNBVYdjXYDFJCaspjkjjoCTRA/edit?usp=sharing",
+            buttonText: "View the Sheet!",
+            description: `
+                    <p>
+                        Tracking your applications ensures your organization and success on the job hunt. 
+                        <ul>
+                            <li>Highlight the aspects of the positions you like/dislike to help make your final decision/li>
+                            <li>Remind yourself of the details of the position in the minutes before interviews</li>
+                            <li>Nail down what you care about when interviewing companies, managers, and teams</li>
+                        </ul>
+                    </p>
+                    `,
+            image: "images/applicationTracker.png",
+            imageAlt: "Screenshot of Google Sheet",
+            title: "Application Tracker",
+            subtitle: "Fall 2024",
+        },
+        {
             id: 1,
             buttonLink: "https://docs.google.com/document/d/1zRC5iB2wZHzEImxES3lLQMi7bkVvQNLYWM5-n6q3Rxc/edit?usp=sharing",
             buttonText: "View the Doc!",
